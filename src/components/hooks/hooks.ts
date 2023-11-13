1f8cbefdb448a0a4895108c050dbd58cc1fe16f3
Vlad <Sp0ngbik@gmail.com>
2023-11-13 15:38:10 +0300

    first commit

--- src/components/hooks/hooks.ts
+++
@@ -1,7 +0,0 @@
-// import {AppDispatch, RootState} from "../redux/store";
-// import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-//
-//
-// export const useAppDispatch: () => AppDispatch = useDispatch
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const asd = 123
